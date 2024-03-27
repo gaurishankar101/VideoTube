08e02b597b89510927363d0789cd0ed4ef22db55
gaurishankar101 <165199927+gaurishankar101@users.noreply.github.com>
2024-03-27 23:42:08 +0530

    added file

---
+++ App.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react'
+import SideBar from './pages/SideBar'
+import NavBar from './pages/NavBar'
+import Side from './pages/Side'
+
+const App = () => {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  )
+}
+
+export default App
+
